0649f5640fdde6d2e90083def8695b9d1db8d805
Yevhen <evgeniy.kostrika@gmail.com>
2018-05-14 23:21:53 +0300

    work in progress

--- parabook.go
+++ parabook.go
@@ -3,15 +3,16 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"github.com/ykostr/parabook/src/parser"
+	"github.com/ykostr/parabook/src/pbo"
 	"io/ioutil"
 )
 
 func main() {
 	b, _ := ioutil.ReadFile("book")
-	p := new(parser.ParallelBook)
+	p := new(pbo.ParallelBook)
 	xml.Unmarshal(b, p)
 	for _, v := range p.Tags {
 		fmt.Println(v)
 	}
+	fmt.Println(p.Lang1.Value)
 }
---
+++ src/pbo/pbo.go
@@ -0,0 +1,25 @@
+package pbo
+
+import (
+	"encoding/xml"
+)
+
+type ParallelBook struct {
+	XMLName xml.Name `xml:"ParallelBook"`
+	Lang1   xml.Attr `xml:"lang1,attr"`
+	Author1 xml.Attr `xml:"author1,attr"`
+	Title1  xml.Attr `xml:"title1,attr"`
+	Info1   xml.Attr `xml:"info1,attr"`
+	Lang2   xml.Attr `xml:"lang2,attr"`
+	Author2 xml.Attr `xml:"author2,attr"`
+	Title2  xml.Attr `xml:"title2,attr"`
+	Info2   xml.Attr `xml:"info2,attr"`
+	Info    xml.Attr `xml:"info,attr"`
+	Tags    []Tag    `xml:"p"`
+}
+
+type Tag struct {
+	Level     string `xml:"l,attr"`
+	Source    string `xml:"s,attr"`
+	Translate string `xml:"t,attr"`
+}
